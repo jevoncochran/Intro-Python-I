7e40a85105b9f5cca59920629a285d09792560ae
Jevon Cochran <jevon.cochran@gmail.com>
2020-05-06 19:32:25 -0300

    70 percent

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -26,11 +26,9 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [i.upper() for i in a]
 
-[i.upper() for i in a]
-
-print(a)
+print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
@@ -38,6 +36,6 @@ print(a)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [elem for elem in x if int(elem) % 2 == 0]
 
 print(y)
